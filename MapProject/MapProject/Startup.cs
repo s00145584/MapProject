6a5e3bbfc75c9efaf5e408ada6cbfa5910dfdada
s00145584 <s00145584@mail.itsligo.ie>
2016-11-05 19:11:58 +0000

    release1

---
+++ MapProject/MapProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MapProject.Startup))]
+namespace MapProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
